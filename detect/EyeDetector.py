58982a7ea13442bbc1fc794d0b50162cfac5e494
Austin Edwards <edwardsa@janelia.hhmi.org>
2014-06-02 18:29:31 -0400

    shifting to object oriented, finished EyeCascade

---
+++ detect/EyeDetector.py
@@ -0,0 +1,24 @@
+import cv2
+import os
+import np
+
+class EyeDetector:
+
+    def __init__(self,cascade_fn="../cascades/rightEyeCascade.xml",minNeighbors=5,minSize=(100,100)):
+        if not os.path.exists(cascade_fn):
+            raise IOError("Cascade not found in path %s." % cascade_fn)
+        self.cascade = cv2.CascadeClassifier(cascade)
+        self.minNeighbors = minNeighbors
+        self.minSize = minSize
+
+    def detect(self, image):
+        if np.ndim(image) == 3:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Detect eyes in image 
+        eyes = self.cascade.detectMultiScale(image,minNeighbors=self.minNeighbors,minSize=self.minSize)
+
+        if len(eyes) != 1:
+            return []
+
+        else: return image[y:y+h, x:x+w]
