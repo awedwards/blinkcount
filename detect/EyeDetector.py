0c244279093f36ab24a6d3f7c227e8fd503f2676
Austin Edwards <austinedwards@austins-mbp.home>
2014-06-02 21:43:42 -0400

    debugged EyeDetector

--- detect/EyeDetector.py
+++ detect/EyeDetector.py
@@ -1,13 +1,13 @@
 import cv2
 import os
-import np
+import numpy as np
 
 class EyeDetector:
 
-    def __init__(self,cascade_fn="../cascades/rightEyeCascade.xml",minNeighbors=5,minSize=(100,100)):
+    def __init__(self,cascade_fn="../cascades/rightEyeCascade.xml", minNeighbors=5, minSize=(100,100)):
         if not os.path.exists(cascade_fn):
             raise IOError("Cascade not found in path %s." % cascade_fn)
-        self.cascade = cv2.CascadeClassifier(cascade)
+        self.cascade = cv2.CascadeClassifier(cascade_fn)
         self.minNeighbors = minNeighbors
         self.minSize = minSize
 
@@ -16,9 +16,11 @@ class EyeDetector:
             image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         # Detect eyes in image 
-        eyes = self.cascade.detectMultiScale(image,minNeighbors=self.minNeighbors,minSize=self.minSize)
+        eyes = self.cascade.detectMultiScale(image, minNeighbors=self.minNeighbors,minSize=self.minSize)
 
         if len(eyes) != 1:
             return []
 
-        else: return image[y:y+h, x:x+w]
+        else:
+            (x,y,w,h) = eyes[0]
+            return image[y:y+h, x:x+w]
