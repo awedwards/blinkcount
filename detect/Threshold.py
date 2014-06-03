11b197113ae2e1193ff37425b219a5a66c3515c2
Austin Edwards <edwardsa@janelia.hhmi.org>
2014-06-03 17:24:58 -0400

    debugged Threshold

--- detect/Threshold.py
+++ detect/Threshold.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import math
 
-class Threshold(image):
+class Threshold:
     # img = frame from videocapture
 
     def __init__(self, t=0, m=0, w=0, h=0):
@@ -17,14 +17,14 @@ class Threshold(image):
             image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         #get size, max intensity of the gray image
-        self.w, self.h = gray.shape[:2]
-        self.m = np.amax(gray)
+        self.w, self.h = image.shape[:2]
+        self.m = np.amax(image)
 
         #Calculate target thresholded image
         target = (self.w*self.h*255)/2
 
         #initialize t, tnew (threshold values)
-        tnew = math.floor(m/2)
+        tnew = math.floor(self.m/2)
         t = 0
 
         while (t-tnew != 0):
@@ -46,3 +46,4 @@ class Threshold(image):
 
         ret, th = cv2.threshold(image,self.t,self.m,cv2.THRESH_BINARY)
         return th
+
